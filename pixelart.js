88e589760a472208dd55936c21b8e32b6242dd27
JoaquinGonzalezVandam <joaquin.gonzalez@alu.bue.edu.ar>
2016-11-07 11:34:31 -0300

    Version 2

---
+++ pixelart.js
@@ -0,0 +1,58 @@
+var myPixelDraw ={
+	colorPicked: 	0,
+	cellColor:  	"#ecf0f1",
+	defaultCells: 	30,
+	coloring: 		false,
+
+	init: function(container){
+		this.container = container;
+		var keys = Object.keys(this.fns);
+		console.log(keys);
+		for (var i = 0; i < keys.lenght; i++) {
+
+		}
+	},
+	
+	fns: {
+		calcSize: function(cantidadCeldas){
+			var tamañoDeseado = cantidadCeldas * cantidadCeldas;
+			$(".container").empty();
+			for (var i = 0; i < tamañoDeseado ; i++) {
+					$(".container").append('<div class: "cell" draggable></div>');
+				}
+		},
+		reSize: function(){
+			console.log("reSize");
+		},
+		detectMouseUp: function(){
+			console.log("detectMouseUp");
+		},
+		colorPalette: function(){
+			console.log("colorPalette");
+		},
+		pickColor: function(){
+			console.log("pickColor");
+		},
+		colorIt: function(){
+			console.log("colorIt");
+		},
+		colorOnDrag: function(){
+			console.log("colorOnDrag");
+		},
+		reset: function(){
+			console.log("reset");
+		},
+		toggleBorders: function(){
+			console.log("toggleBorders");
+		},
+		disableRightClick: function(){
+			console.log("disableRightClick");
+		},
+		grabImage: function(){
+			console.log("grabImage");
+		},
+	}
+};
+$(document).ready(function(){
+	myPixelDraw.init($("#container"));
+});
